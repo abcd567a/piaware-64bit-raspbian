2cb1ac64fdbdd18e3427fe819d2c9c56f79c74a8
abcd567a <abcd567@hotmail.com>
2020-10-04 02:13:47 -0400

    Create build-and-install-piaware.sh

---
+++ build-and-install-piaware.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+INSTALL_DIRECTORY=${PWD}
+
+echo -e "\e[32mUpdating\e[39m"
+sudo apt update
+echo -e "\e[32mInstalling build tools\e[39m"
+sudo apt install -y debhelper dh-systemd
+
+echo -e "\e[32mBuilding & Installing tcl-tls from source code. \e[39m"
+echo -e "\e[32mInstalling tcl-tls dependencies \e[39m"
+sudo apt install -y libssl-dev tcl-dev chrpath
+echo -e "\e[32mCloning tcl-tls source code \e[39m"
+
+cd  ${INSTALL_DIRECTORY}
+git clone http://github.com/flightaware/tcltls-rebuild.git
+echo -e "\e[32mbuilding tcl-tls package \e[39m"
+cd  ${INSTALL_DIRECTORY}/tcltls-rebuild
+./prepare-build.sh buster
+cd package-buster
+sudo dpkg-buildpackage -b --no-sign
+echo -e "\e[32mInstalling tcl-tls package \e[39m"
+cd ../
+sudo dpkg -i tcl-tls_*.deb
+
+echo -e "\e[32mPutting tcl-tls package on hold to prevent its replacement by repository during upgrade \e[39m"
+sudo apt-mark hold tcl-tls
+tcl-tls set on hold.
+
+echo -e "\e[32mInstalling piaware dependencies \e[39m"
+sudo apt install -y libboost-system-dev libboost-program-options-dev
+sudo apt install -y libboost-regex-dev libboost-filesystem-dev
+sudo apt install -y tclx8.4 tcllib itcl3
+
+
+echo -e "\e[32mCloning piaware source code and building package \e[39m"
+cd ${INSTALL_DIRECTORY}
+git clone http://github.com/flightaware/piaware_builder
+cd ${INSTALL_DIRECTORY}/piaware_builder
+sudo ./sensible-build.sh buster
+
+
+
+echo -e "\e[32mbuilding the piaware package \e[39m"
+cd ${INSTALL_DIRECTORY}/piaware_builder/package-buster
+sudo dpkg-buildpackage -b --no-sign
+
+echo -e "\e[32mInstalling piaware package\e[39m"
+cd ../
+sudo dpkg -i piaware_*.deb
+
+echo ""
+echo -e "\e[32mPIAWARE INSTALLATION COMPLETED \e[39m"
+echo ""
+echo -e "\e[39mIf you already have  feeder-id, please configure piaware with it \e[39m"
+echo -e "\e[39mFeeder Id is available on this address while loggedin: \e[39m"
+echo -e "\e[94m    https://flightaware.com/adsb/stats/user/ \e[39m"
+echo ""
+echo -e "\e[39m    sudo piaware-config feeder-id xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx \e[39m"
+echo -e "\e[39m    sudo piaware-config allow-manual-updates yes \e[39m"
+echo -e "\e[39m    sudo piaware-config allow-auto-updates yes \e[39m"
+echo -e "\e[39m    sudo systemctl restart piaware \e[39m"
+echo ""
+echo -e "\e[39mIf you dont already have a feeder-id, please go to Flightaware Claim page while loggedin \e[39m"
+echo -e "\e[94m    https://flightaware.com/adsb/piaware/claim \e[39m"
+echo ""
